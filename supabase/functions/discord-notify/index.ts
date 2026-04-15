32d016ee3c0e4db990b0e1de288d91c14f61845a
AntonioPalmaDev <antoniopalma18@outlook.com>
2026-04-15 16:32:21 -0300

    add dnv

--- supabase/functions/discord-notify/index.ts
+++ supabase/functions/discord-notify/index.ts
@@ -8,8 +8,7 @@ Deno.serve(async (req: Request) => {
     return new Response('ok', { headers: corsHeaders })
   }
 
-  const DISCORD_WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1494055290922864642/GWjUm--wBUmExKbRJMYCadpvtiTY5FHxlGvIPdRtwOUNbwU2BbYzEWgRhfrLt9J4ZU2-'
-
+const DISCORD_WEBHOOK_URL = Deno.env.get('https://discordapp.com/api/webhooks/1494055290922864642/GWjUm--wBUmExKbRJMYCadpvtiTY5FHxlGvIPdRtwOUNbwU2BbYzEWgRhfrLt9J4ZU2-') ?? ''
   //teste
   function formatDate(date: Date): string {
     const pad = (n: number) => n.toString().padStart(2, '0')
